b649dc1038a913f098dfb1b61b37d23d57c44203
mahalabsi <mahatalabsi@gmail.com>
2024-12-10 11:12:22 +0300

    delete works

--- controllers/booklists.js
+++ controllers/booklists.js
@@ -3,34 +3,6 @@ const router = express.Router();
 const Book = require('../models/book');  // Your Book model
 const Userbooklist = require('../models/userbooklist');  // Your Userbooklist model
 
-// Render the user's booklist page
-// router.get('/booklist', async (req, res) => {
-//   const userId = req.session.user._id;
-
-//   try {
-//     // Find the user's booklist and populate book names
-//     const userBookList = await Userbooklist.findOne({ user: userId }).populate('bookName');
-
-//     if (!userBookList || !userBookList.bookName.length) {
-//       return res.render('books/booklist.ejs', { books: [], statusOptions: ['Want to Read', 'Currently Reading', 'Finished Reading'] }); // If no books, send an empty array
-//     }
-
-//     // Get the book details from the populated bookName field
-//     const books = userBookList.bookName.map(book => ({
-//       name: book.name,
-//       image: book.img,
-//       status: userBookList.readingStatus, 
-//       _id: book._id,
-//     })); 
-
-
-//     // Pass books and status options to the view
-//     res.render('books/booklist.ejs', { books, statusOptions: ['Want to Read', 'Currently Reading', 'Finished Reading'] });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Error fetching book list');
-//   }
-// });
 
 router.get('/booklist', async (req, res) => {
   const userId = req.session.user._id;
@@ -42,6 +14,7 @@ router.get('/booklist', async (req, res) => {
     if (!userBookList || userBookList.length === 0) {
       return res.render('books/booklist.ejs', { books: [], statusOptions: ['Want to Read', 'Currently Reading', 'Finished Reading'] });
     }
+   
 
     // Get the book details and map them with their respective reading status
     const books = userBookList.map(entry => ({
@@ -50,54 +23,18 @@ router.get('/booklist', async (req, res) => {
       status: entry.readingStatus, // Status for each book entry
       _id: entry.bookName._id,
     }));
-
+    // console.log(books)
+    // const userBookid = await Book.findOne({  bookName: books._id});
+    // console.log(userBookid.id)
+    // userBookid:userBookid.id,
     // Pass books and status options to the view
-    res.render('books/booklist.ejs', { books, statusOptions: ['Want to Read', 'Currently Reading', 'Finished Reading'] });
+    res.render('books/booklist.ejs', { books,  statusOptions: ['Want to Read', 'Currently Reading', 'Finished Reading'] });
   } catch (error) {
     console.error(error);
     res.status(500).send('Error fetching book list');
   }
 });
 
-
-// // updating the book's status
-// router.post('/booklist', async (req, res) => {
-//   const { status, bookId } = req.body; 
-//   const userId = req.session.user._id;
-
-
-//   console.log("bookId", bookId)
-//   try {
-//     // Find the user's book list
-//     // const userBookList = await Userbooklist.findById(bookId).populate('bookName');
-//     // console.log(userBookList)
-
-//     const userBookList = await Userbooklist.find({user: userId})
-//     console.log(userBookList)
-    
-//     if (!userBookList) {
-//       return res.status(404).send('User book list not found');
-//     }
-        
-//     userBookList.readingStatus = status; 
-//     // await userBookList.save();
-
-//     // After saving the updated book list, render the updated booklist.ejs
-//     const updatedUserBookList = await Userbooklist.findOne({ user: userId }).populate('bookName');
-//     const books = updatedUserBookList.bookName.map(book => ({
-//       name: book.name,
-//       image: book.img,
-//       status: updatedUserBookList.readingStatus,  
-//     }));
-
-   
-//     res.render('books/booklist.ejs', { books, statusOptions: ['Want to Read', 'Currently Reading', 'Finished Reading'] });
-
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Error updating book status');
-//   }
-// });
 
 router.post('/booklist', async (req, res) => {
   const { status, bookId } = req.body; 
@@ -109,6 +46,7 @@ router.post('/booklist', async (req, res) => {
   try {
     // Find the specific Userbooklist entry for the given user and book
     const userBook = await Userbooklist.findOne({ user: userId, bookName: bookId });
+  
 
     if (!userBook) {
       return res.status(404).send('User book list entry not found');
@@ -131,6 +69,7 @@ router.post('/booklist', async (req, res) => {
       _id: entry.bookName._id,
     }));
 
+
     // Render the updated book list with status options
     res.render('books/booklist.ejs', { books, statusOptions: ['Want to Read', 'Currently Reading', 'Finished Reading'] });
 
@@ -139,6 +78,61 @@ router.post('/booklist', async (req, res) => {
     res.status(500).send('Error updating book status');
   }
 });
+// console.log(bookId)
+  
+
+
+
+// router.delete('/booklist', async (req, res) => {
+//   const userId = req.session.user._id;  
+//   const bookId = req.body.bookId;  
+//   console.log(userId);
+//   console.log(`hello ${bookId}`);
+
+//   try {
+//     // Find the user's book list by the user ID
+//     const bookList = await Userbooklist.findOne({ user: userId, bookName: bookId });
+
+//     if (bookList) {
+//       await Userbooklist.deleteOne({ user: userId, bookName: bookId })
+    
+//       console.log('Book removed from the list');
+//     }
+
+//     res.redirect('/books/book-page');  
+
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send('Error removing book from list');
+
+//   }
+//   res.render('books/booklist.ejs')
+// });
+router.delete('/booklist/:id', async (req, res) => {
+  const userId = req.session.user?._id;  
+  const bookId = req.params.id;  
+
+  if (!userId || !bookId) {
+    return res.status(400).send('Missing user or book ID');
+  }
+
+  try {
+    // Find and delete the book entry
+    const bookEntry = await Userbooklist.findOneAndDelete({ user: userId, bookName: bookId });
+
+    if (!bookEntry) {
+      return res.status(404).send('Book not found in the list');
+    }
+
+    console.log('Book removed from the list');
+    res.redirect('/booklists/booklist');  
+
+  } catch (error) {
+    console.error('Error removing book:', error);
+    res.status(500).send('Error removing book from list');
+  }
+});
+
 
 
 
